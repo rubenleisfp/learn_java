49f752d142541882d092c060edf7677a2bfd4b90
Ruben Leis <rubenleisfp@gmail.com>
2025-03-10 12:36:11 +0100

    Subir a git

---
+++ src/main/java/org/example/entorno/actividad116/HolaMundo.java
@@ -0,0 +1,10 @@
+package org.example.entorno.actividad116;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HolaMundo");
+	}
+
+}
