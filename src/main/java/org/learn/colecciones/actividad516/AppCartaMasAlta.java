1f3ebe89630ec76ccb4fd20765f1c49a4eeead50
Ruben Leis <rubenleisfp@gmail.com>
2025-03-31 19:32:56 +0200

    Juego carta mas alta

--- src/main/java/org/learn/colecciones/actividad516/AppCartaMasAlta.java
+++ src/main/java/org/learn/colecciones/actividad516/AppCartaMasAlta.java
@@ -11,31 +11,7 @@ public class AppCartaMasAlta {
     }
 
     private void run() {
-        Scanner scanner = new Scanner(System.in);
+        //TODO Falta por implementar
 
-        //Obtener el numero de rondas
-        System.out.println("Introduza el numero de rondas: " );
-        String numRondasStr = scanner.nextLine();
-        int numRondas = Integer.parseInt(numRondasStr);
-
-
-        //Obtener los jugadores
-        String nombreJugador = "";
-        List<Jugador> jugadores = new ArrayList<>();
-
-        do {
-            System.out.println("Introduza un nombre de jugador: " );
-
-            nombreJugador= scanner.nextLine();
-
-            // Solo añade el jugador si el nombre no es "FIN"
-            if (!nombreJugador.equalsIgnoreCase("FIN")) {
-                jugadores.add(new Jugador(nombreJugador));
-            }
-        } while (!nombreJugador.equals("FIN"));
-
-        //Arrancar partida
-        Partida partida = new Partida(numRondas,jugadores);
-        partida.comenzar();
     }
 }
